c3e109914efa7e328c08606ff237cdfdea2e265c
Kaustav Banerjee <31596841+kaustavbnrjee@users.noreply.github.com>
2025-06-05 18:56:18 -0400

    Update code_help.py

--- code_help.py
+++ code_help.py
@@ -1,1 +1,5 @@
-
+# Open the terminal in VS Code
+# Cmd + J (Mac)
+which python # to check your environment
+# answer # /opt/anaconda3/bin/python
+python --version # for version # answer is #Python 3.12.4
